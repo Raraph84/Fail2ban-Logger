dcf36ab8248c3680f3720a8ee2e44e150964162b
Raraph84 <contact+github@raraph.fr>
2023-04-04 10:10:13 +0200

    Fixes

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 git clone https://github.com/Raraph84/Fail2ban-Logger
-mv Fail2ban-Logger /usr/bin/fail2ban-Logger
+mv Fail2ban-Logger /usr/bin/fail2ban-logger
+cd /usr/bin/fail2ban-logger
 cp fail2ban-logger.service /etc/systemd/system
+cp config.example.json /usr/bin/fail2ban-logger/config.json
+npm install --production
 systemctl enable --now fail2ban-logger.service
